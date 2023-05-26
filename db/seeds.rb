8af9d7bd8904026cc0e7d1ff3974ec7d77f7a5c9
Arnaud Lejade <arnaud.lejade1@gmail.com>
2023-05-26 11:19:37 +0200

    End of day 1, frontend first draft

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 # Clean DB
 Movie.destroy_all
+List.destroy_all
 
 10.times do
   Movie.create!(
@@ -7,5 +8,13 @@ Movie.destroy_all
     overview: Faker::Lorem.sentence(word_count: 8),
     poster_url: Faker::LoremFlickr.image(size: "300x300"),
     rating: (rand()*10).round(1),
+  )
+end
+
+lists = ["Classic", "Funny", "Adventure", "Horror"]
+
+lists.each do |list|
+  List.create!(
+    name: list
   )
 end
